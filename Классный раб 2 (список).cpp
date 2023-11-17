7382b8b74e403b2699ca1625949f4fae5434e3a8
Alan <57073942+AlanOleg@users.noreply.github.com>
2023-11-17 17:10:09 +0300

    Update Классный раб 2 (список).cpp

--- Классный раб 2 (список).cpp
+++ Классный раб 2 (список).cpp
@@ -2,57 +2,57 @@
 #include <vector>
 using namespace std;
 
-struct Node{
-        int data;
-        Node* next;
+struct Node {
+    int data;
+    Node* next;
 };
 
-class LinkedList{
-    public:
-        LinkedList();
-        LinkedList(int data);
-        LinkedList(vector<int> data);
-        bool add_element(int data);
-        int delete_element(int data);
-        bool find_element(int data);
-        bool insert(int data, int position);
-        void print_list();
-        ~LinkedList();
-    private:
-        Node* first;
-        Node* last;
+class LinkedList {
+public:
+    LinkedList();
+    LinkedList(int data);
+    LinkedList(vector<int> data);
+    bool add_element(int data);
+    bool delete_element(int position);
+    bool find_element(int data);
+    bool insert_element(int data, int position);
+    void print_list();
+    ~LinkedList();
+private:
+    Node* first;
+    Node* last;
 };
 
-LinkedList::~LinkedList(){
+LinkedList::~LinkedList() {
     Node* current = first;
-    while (current != nullptr){
+    while (current != nullptr) {
         Node* next = current->next;
         delete current;
         current = next;
     }
 }
 
-LinkedList::LinkedList(){
+LinkedList::LinkedList() {
     first = nullptr;
     last = nullptr;
 };
 
-LinkedList::LinkedList(int data){
+LinkedList::LinkedList(int data) {
     Node* nd = new Node{data, nullptr};
     first = nd;
     last = nd;
     last->next = nullptr;
 };
 
-LinkedList::LinkedList(vector<int> data){
-    for (int i = 0; i <= data.size(); i++){
+LinkedList::LinkedList(vector<int> data) {
+    for (int i = 0; i < data.size(); i++) {
         add_element(data[i]);
     }
 };
 
-bool LinkedList::add_element(int data){
-    Node* nd = new Node{data, nullptr};
-    if(last == nullptr){
+bool LinkedList::add_element(int data) {
+    Node* nd = new Node{ data, nullptr };
+    if (last == nullptr) {
         first = nd;
         last = nd;
         return true;
@@ -62,13 +62,13 @@ bool LinkedList::add_element(int data){
     return true;
 };
 
-bool LinkedList::find_element(int data){
-    if (first == nullptr){
+bool LinkedList::find_element(int data) {
+    if (first == nullptr) {
         return 0;
     }
     Node* current = first;
-    while(current != nullptr){
-        if (current->data == data){
+    while (current != nullptr) {
+        if (current->data == data) {
             return 1;
         }
         current = current->next;
@@ -76,44 +76,82 @@ bool LinkedList::find_element(int data){
     return 0;
 };
 
-bool LinkedList::insert(int data, int position){
-    if (first == nullptr and position == 0){
+bool LinkedList::insert_element(int data, int position) {
+    if (position < 0 or first == nullptr and position > 0){
+        cout << "incorrect position" << endl;
+        return 0;
+    }
+    else if (first == nullptr and position == 0) {
         add_element(data);
         return 1;
     }
-    else if (position == 0){
-        Node* nd = new Node{data, first};
+    else if (position == 0) {
+        Node* nd = new Node{ data, first };
         first = nd;
         return 1;
     }
-    else{
+    else {
         Node* current = first;
-        for (int i = 1; i < position; i++){
+        for (int i = 1; i < position; i++) {
             current = current->next;
-            if (current->next == nullptr){
+            if (current == nullptr or current->next == nullptr) {
                 cout << "incorrect position" << endl;
                 return 0;
             }
         }
         Node* nd = new Node{data, current->next};
         current->next = nd;
-        if (nd->next == nullptr){
+        if (nd->next == nullptr) {
             last = nd;
         }
         return 1;
     }
 };
 
-void LinkedList::print_list(){
-    if (first == nullptr){
-        cout << "list is empty";
+bool LinkedList::delete_element(int position) {
+    if (first == nullptr or first == last and position != 0 or position < 0) {
+        return 0;
     }
-    else if (first == last){
+    else if (first == last) {
+        delete first;
+        first = nullptr;
+        last = nullptr;
+        return 1;
+    }
+    else if (position == 0) {
+        Node* current = first->next;
+        delete first;
+        first = current;
+        return 1;
+    }
+    else {
+        Node* current = first;
+        for (int i = 1; i < position; i++) {
+            current = current->next;
+            if (current == nullptr or current->next == nullptr) {
+                return 0;
+            }
+        }
+        Node* placeholder = current->next;
+        current->next = current->next->next;
+        delete placeholder;
+        if (current->next == nullptr) {
+            last = current;
+        }
+        return 1;
+    }
+};
+
+void LinkedList::print_list() {
+    if (first == nullptr) {
+        cout << "list is empty" << endl;
+    }
+    else if (first == last) {
         cout << first->data << endl;
     }
-    else{
+    else {
         Node* current = first;
-        while(current->next != nullptr){
+        while (current != nullptr) {
             cout << current->data << " ";
             current = current->next;
         }
@@ -123,12 +161,22 @@ void LinkedList::print_list(){
 
 int main()
 {
-    vector<int> massiv = {1, 2, 3};
+    int amount;
+    vector<int> massiv = {};
+    cout << "amount = "; cin >> amount;
+    for (int i = 0; i < amount; i++) {
+        int number;
+        cout << "number " << i << " = "; cin >> number;
+        massiv.push_back(number);
+    }
     LinkedList linkedlist(massiv);
-    int cin_data;
-    int cin_position;
-    cout << "data = "; cin >> cin_data;
-    cout << "position = "; cin >> cin_position;
-    linkedlist.insert(cin_data, cin_position);
     linkedlist.print_list();
+    //int cin_data;
+    int cin_position;
+    //cout << "data = "; cin >> cin_data;
+    cout << "position = "; cin >> cin_position;
+    //linkedlist.insert_element(cin_data, cin_position);
+    linkedlist.delete_element(cin_position);
+    linkedlist.print_list();
+    //cout << linkedlist.first->data << " " << linkedlist.last->data;
 }
